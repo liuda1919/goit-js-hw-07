b525ffed21a17ec91c7cc3ce0b61304a3db9dd37
liuda1919 <121105146+liuda1919@users.noreply.github.com>
2023-05-01 14:32:58 +0300

    update js & html

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 const galleryList = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
