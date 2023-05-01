cb78b3c1f4a4cac3a9275801ef964ba8ef9d7d68
liuda1919 <121105146+liuda1919@users.noreply.github.com>
2023-05-01 13:29:12 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+function createGalleryMarkup(items) {
+    return items
+    .map(
+        ({ preview, original, description }) =>
+            `<li class = "gallery__item">
+            <a class="gallery__link" href = "${original}">
+            <img 
+             class="gallery__image"
+               src="${preview}"
+               data-source="${original}"
+               alt="${description}"
+             />
+           </a>
+         </li>`
+    )
+        .join('');
+}
 
+galleryList.addEventListener('click', onGalleryItemClick); 
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const urlImg = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${urlImg}">`);
+    instance.show();
+    document.addEventListener('keydown', onEscKeyPress);
+
+
+    function onEscKeyPress(event) {
+        if (event.code === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', onEscKeyPress);
+        }
+    }
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+function createGalleryMarkup(items) {
+    return items
+    .map(
+        ({ preview, original, description }) =>
+            `<li class = "gallery__item">
+            <a class="gallery__link" href = "${original}">
+            <img 
+             class="gallery__image"
+               src="${preview}"
+               alt="${description}"
+             />
+           </a>
+         </li>`
+    )
+        .join('');
+}
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'bottom'
+});
 
 console.log(galleryItems);
